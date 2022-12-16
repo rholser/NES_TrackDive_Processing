4f76cc207784aee95a5dbb1256f674339b0b9266
abfavilla <63368687+abfavilla@users.noreply.github.com>
2022-12-16 13:35:50 -0800

    added code for Kami processing adjusted da zoc parameters to account for large zero offsets

--- Step 3 Process Diving Data/change_format_DA2_RRH_TV4_2.m
+++ Step 3 Process Diving Data/change_format_DA2_RRH_TV4_2.m
@@ -19,6 +19,15 @@
 %
 % Update: 10-Dec-2022
 % Add datetime conversions for different instrument types
+%          5-Dec-2022 - modified by Arina Favilla to (1) update zoc approach to account for large zero offset 
+%                       and (2) fix Kami timestamp:
+%                           added elseif statements to Step 1
+%                           added line to Step 3.1
+%                           added Step 3.3
+%                           added code to Kami if statement in Step 4
+%                           modified Step 8 to round DepthRes
+%                           added Step 8.1
+%                           edited Step 9
 
 function output=change_format_DA2_RRH_TV4_2(filename,Start,End,TOPPID)
     %Step 1: load csv of TDR data
@@ -73,11 +82,19 @@ function output=change_format_DA2_RRH_TV4_2(filename,Start,End,TOPPID)
     
     %Step 3: date conversions
     %Step 3.1: parse out time into separate columns using datevec
-    try
-        data.Time=datetime(data.Time,'InputFormat','HH:mm:ss dd-MMM-yyyy');
-    end
-    try
-        data.Time=datetime(data.Time,'InputFormat','dd/MM/yyyy HH:mm:ss');
+    if size(strfind(filename,'out-Archive'),1)>0
+        try
+            data.Time=datetime(data.Time,'InputFormat','dd/MM/yyyy HH:mm:ss');
+        end
+        try
+            data.Time=datetime(data.Time,'InputFormat','HH:mm:ss dd-MMM-yyyy');
+        end
+    elseif size(strfind(filename,'Kami'),1)>0
+        [y m d]=ymd(data.Date);
+        [H M S]=hms(data.Time);
+        data.Time=datetime(y,m,d,H,M,S);
+        data=removevars(data,'Date');
+        clear y m d H M S
     end
     %try
     %    data.Time=data.Date+data.Time;
@@ -128,10 +145,148 @@ function output=change_format_DA2_RRH_TV4_2(filename,Start,End,TOPPID)
                 data.Time=data.time+years(4);
             end
         end
+
+        %Step 3.3: save UTC offset and compression factor to Kami records
+        %(applied in Step 4)
+        if size(strfind(filename,'Kami'),1)>0
+            if TOPPID==2011017
+                offset = hours(7)+minutes(58)+seconds(57);
+                compress = minutes(2)+seconds(10); 
+                Cut=datetime(2011,4,7,4,25,55)+offset;
+            elseif TOPPID==2011019
+                offset = hours(7)+minutes(59)+seconds(54);
+                compress = minutes(3)+seconds(6);
+                Cut=0; 
+            elseif TOPPID==2011020
+                offset = hours(7)+minutes(59)+seconds(11);
+                compress = minutes(4)+seconds(26);
+                Cut=0; 
+            elseif TOPPID==2012006
+                offset = hours(8)+minutes(0)+seconds(34);
+                compress = minutes(3)+seconds(21);
+                Cut=0; 
+            elseif TOPPID==2012012
+                offset = hours(7)+minutes(50)+seconds(22);
+                compress = minutes(1)+seconds(45);
+                Cut=datetime(2012,3,8,23,5,35)+offset;
+            elseif TOPPID==2012038
+                offset = hours(5)+minutes(41)+seconds(0);
+                compress = minutes(5)+seconds(57);
+                Cut=datetime(2012,9,20,12,11,15)+offset;
+            elseif TOPPID==2013005
+                offset = hours(8)+minutes(0)+seconds(38);
+                compress = minutes(1)+seconds(48);
+                Cut=0; 
+            elseif TOPPID==2013006
+                offset = hours(8)+minutes(0)+seconds(30);
+                compress = seconds(10);
+                Cut=0; 
+            elseif TOPPID==2013008
+                offset = hours(8)+minutes(0)+seconds(56);
+                compress = minutes(3)+seconds(10);
+                Cut=0; 
+            elseif TOPPID==2013009
+                offset = hours(8)+minutes(1)+seconds(3);
+                compress = minutes(2)+seconds(46);
+                Cut=0; 
+            elseif TOPPID==2013011
+                offset = hours(8)+minutes(0)+seconds(14);
+                compress = minutes(2)+seconds(45);
+                Cut=0; 
+            elseif TOPPID==2013014
+                offset = hours(7)+minutes(52)+seconds(16);
+                compress = minutes(1)+seconds(24);
+                Cut=0; 
+            elseif TOPPID==2013015
+                offset = hours(8)+minutes(0)+seconds(16);
+                compress = minutes(3)+seconds(21);
+                Cut=0; 
+            elseif TOPPID==2013029
+                offset = hours(5)+minutes(41)+seconds(43);
+                compress = minutes(6)+seconds(0);
+                Cut=datetime(2013,10,29,9,55,30)+offset;
+            elseif TOPPID==2013030
+                offset = hours(6)+minutes(59)+seconds(45); 
+                compress = minutes(7)+seconds(20);
+                Cut=datetime(2013,12,25,13,22,55)+offset;                
+            elseif TOPPID==2013031
+                offset = hours(6)+minutes(59)+seconds(39);
+                compress = minutes(9)+seconds(45);
+                Cut=datetime(2013,12,26,15,00,55)+offset;
+            elseif TOPPID==2013032
+                offset = hours(6)+minutes(59)+seconds(0);
+                compress = minutes(17)+seconds(29);
+                Cut=datetime(2013,12,26,15,00,55)+offset;
+            elseif TOPPID==2013033
+                offset = hours(6)+minutes(59)+seconds(20); 
+                compress = minutes(10)+seconds(30); 
+                Cut=datetime(2013,12,30,10,5,0)+offset;  
+            elseif TOPPID==2014011
+                offset = hours(7)+minutes(41)+seconds(2);
+                compress = minutes(2)+seconds(32); 
+                Cut=0; 
+            elseif TOPPID==2014013
+                offset = hours(8)+minutes(0)+seconds(28);
+                compress = minutes(4)+seconds(46);
+                Cut=0; 
+            elseif TOPPID==2014018
+                offset = hours(8)+minutes(0)+seconds(20);
+                compress = minutes(4)+seconds(15); 
+                Cut=0; 
+            elseif TOPPID==2015003
+                offset = hours(7)+minutes(45)+seconds(47);
+                compress = minutes(3)+seconds(29); 
+                Cut=0; 
+            else
+                offset=0; 
+                compress=0; 
+                Cut=0; 
+            end
+        end
     
     %Step 4: truncate record to start and end time for TDRs that are not
-        %from Kami or Stroke accelerometers
+        %from Stroke accelerometers
+        % for Kami TDRs: apply offset, truncate record, and apply 
+        % compression factor 
         if size(strfind(filename,'Kami'),1)>0
+            if isduration(offset)
+                data.Time=data.Time+offset;
+                [data.Year, data.Month, data.Day, data.Hour, data.Min, data.Sec]...
+                    =datevec(data.Time);
+                data.Sec=round(data.Sec); % some seconds > 59 so next lines fix the issue
+                % nextmin=find(data.Sec>59); 
+                if sum(data.Sec>59)>0
+                    data.Time=datetime(data.Year, data.Month, data.Day, data.Hour, data.Min, data.Sec);
+                    [data.Year, data.Month, data.Day, data.Hour, data.Min, data.Sec]...
+                        =datevec(data.Time);
+                end
+            end
+
+            if isdatetime(Cut) % QC to make sure this works
+                data(data.Time>Cut,:)=[];
+                [~,ind1]=min(abs(data.Time-Start));
+                [~,ind2]=min(abs(data.Time-End));
+                data=data(ind1:ind2,:);
+            else 
+                [~,ind1]=min(abs(data.Time-Start));
+                [~,ind2]=min(abs(data.Time-End));
+                data=data(ind1:ind2,:);
+            end
+
+            if isduration(compress)
+                record_length=seconds(data.Time(end)-data.Time(1));
+                record_frac=seconds(data.Time(:)-data.Time(1));
+                data.Time=data.Time-seconds((seconds(compress)*record_frac)/record_length); 
+                [data.Year, data.Month, data.Day, data.Hour, data.Min, data.Sec]...
+                    =datevec(data.Time);       
+                data.Sec=round(data.Sec); % some seconds > 59 so next lines fix the issue
+                if sum(data.Sec>59)>0
+                    data.Time=datetime(data.Year, data.Month, data.Day, data.Hour, data.Min, data.Sec);
+                    [data.Year, data.Month, data.Day, data.Hour, data.Min, data.Sec]...
+                        =datevec(data.Time);
+                end
+            end
+
         elseif size(strfind(filename,'Stroke'),1)>0
         else
             [~,ind1]=min(abs(data.Time-Start));
@@ -174,15 +329,44 @@ function output=change_format_DA2_RRH_TV4_2(filename,Start,End,TOPPID)
         else
             DepthRes=DepthRes(2,1);
         end
+        if rem(DepthRes,0.5)>0 % added by Arina to deal with Kami DepthRes as 1.2 m
+            DepthRes=floor(DepthRes); 
+        end
 
-    %Step 9: Run IKNOS DA    
+        %Step 8.1: detect if dive surface intervals have offset >10m
+        running_surface = movmin(data.Depth,hours(24*2),'SamplePoints',data.Time);
+        [f,xi]=ecdf(running_surface); %figure; ecdf(running_surface,'Bounds','on');
+        minsurface=interp1(f,xi,0.05);
+
+    %Step 9: Run IKNOS DA - new ZocMinMax with DEFAULT ZOC params
+    if minsurface<-10
         if size(strfind(filename,'-out-Archive'),1)>0
             yt_iknos_da_RRH([strtok(filename,'-') '_DAprep_full.csv'],DAstring,...
-            32/SamplingRate,15/DepthRes,20,'wantfile_yes','ZocWindow',2,...
-            'ZocWidthForMode',15/DepthRes,'ZocSurfWidth',10,'ZocDiveSurf',15,'ZocMinMax',[-10,2200]);
+                32/SamplingRate,15/DepthRes,20,'wantfile_yes',...
+                'ZocMinMax',[minsurface-10,2500]);
         else
             yt_iknos_da_RRH([strtok(filename,'.') '_DAprep_full.csv'],DAstring,...
-            32/SamplingRate,15/DepthRes,20,'wantfile_yes','ZocWindow',2,...
-            'ZocWidthForMode',15,'ZocSurfWidth',10,'ZocDiveSurf',15,'ZocMinMax',[-10,2200]);
+                32/SamplingRate,15/DepthRes,20,'wantfile_yes',...
+                'ZocMinMax',[minsurface-10,2500]);
         end
+    else
+        if size(strfind(filename,'-out-Archive'),1)>0
+            yt_iknos_da_RRH([strtok(filename,'-') '_DAprep_full.csv'],DAstring,...
+                32/SamplingRate,15/DepthRes,20,'wantfile_yes');
+        else
+            yt_iknos_da_RRH([strtok(filename,'.') '_DAprep_full.csv'],DAstring,...
+                32/SamplingRate,15/DepthRes,20,'wantfile_yes');
+        end
+    end
+
+%     %Step 9: Run IKNOS DA    
+%         if size(strfind(filename,'-out-Archive'),1)>0
+%             yt_iknos_da_RRH([strtok(filename,'-') '_DAprep_full.csv'],DAstring,...
+%             32/SamplingRate,15/DepthRes,20,'wantfile_yes','ZocWindow',2,...
+%             'ZocWidthForMode',15/DepthRes,'ZocSurfWidth',10,'ZocDiveSurf',15,'ZocMinMax',[-10,2200]);
+%         else
+%             yt_iknos_da_RRH([strtok(filename,'.') '_DAprep_full.csv'],DAstring,...
+%             32/SamplingRate,15/DepthRes,20,'wantfile_yes','ZocWindow',2,...
+%             'ZocWidthForMode',15,'ZocSurfWidth',10,'ZocDiveSurf',15,'ZocMinMax',[-10,2200]);
+%         end
 end
