b6371e49419fac0b0cdf8406e889994360660a8b
Rachel Holser <rholser@ucsc.edu>
2022-12-16 13:08:56 -0800

    removed New Iknos

--- Step 4 Geolocate Dives/GeolocateDives_RRH.m
+++ Step 4 Geolocate Dives/GeolocateDives_RRH.m
@@ -1,7 +1,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Created by: Rachel Holser (rholser@ucsc.edu)
 %Created on: 22-Aug-2022
-%Last Updated: 30-Aug-2022
+%Updates Log: 
+% 16-Dec-2022 - added geolocation for TDR2 and TDR3 files.
 %
 %Required functions:  yt_interpol_linear_2 (IKNOS toolbox)
 %                     SolarAzEl
@@ -22,17 +23,21 @@ files=dir('*_DiveStat.csv');
 first=find(MetaDataAll.TOPPID==str2double(strtok(files(1).name,'_')));
 last=find(MetaDataAll.TOPPID==str2double(strtok(files(end).name,'_')));
 clear files
-for i=first:last 
-    
+for i=first:last
+
     disp([MetaDataAll.FieldID{i} '_' num2str(MetaDataAll.TOPPID(i))])
-    
+
     %find filenames with the current TOPPID
     trackfile=TrackFoieGrasFiles.filename(TrackFoieGrasFiles.TOPPID==MetaDataAll.TOPPID(i));
     tdr1file=TDRDiveStatFiles.filename(TDRDiveStatFiles.TOPPID==MetaDataAll.TOPPID(i));
     tdr1subfile=TDRSubDiveStatFiles.filename(TDRSubDiveStatFiles.TOPPID==MetaDataAll.TOPPID(i));
-    
+    tdr2file=TDR2DiveStatFiles.filename(TDR2DiveStatFiles.TOPPID==MetaDataAll.TOPPID(i));
+    tdr2subfile=TDR2SubDiveStatFiles.filename(TDR2SubDiveStatFiles.TOPPID==MetaDataAll.TOPPID(i));
+    tdr3file=TDR3DiveStatFiles.filename(TDR3DiveStatFiles.TOPPID==MetaDataAll.TOPPID(i));
+    tdr3subfile=TDR3SubDiveStatFiles.filename(TDR3SubDiveStatFiles.TOPPID==MetaDataAll.TOPPID(i));
+
     if ~isempty(trackfile)
-        
+
         %Load foieGras processed track
         Track=readtable(trackfile);
         try
@@ -43,7 +48,7 @@ for i=first:last
             'Lat','x','y','x_se_km','y_se_km','u','v','u_se_km','v_se_km','s','s_se'};
         %Convery DateTime to JulDate
         Track.JulDate=datenum(Track.DateTime);
-        
+
         %load DiveStat files and interpolate track to it
         if ~isempty(tdr1file)
             for j=1:size(tdr1file,1)
@@ -75,7 +80,7 @@ for i=first:last
             end
             clear j
         end
-        
+
         %Repeat the above steps for subsampled TDR data files
         if ~isempty(tdr1subfile)
             for j=1:size(tdr1subfile,1)
@@ -107,126 +112,184 @@ for i=first:last
             end
             clear j
         end
+                %load DiveStat2 files and interpolate track to it
+        if ~isempty(tdr2file)
+            for j=1:size(tdr2file,1)
+                %load dive statistics file
+                Dive1Stat=readtable(tdr2file(j));
+                %Lat/lon from linear interpolation of processed track based on time
+                Dive1LatLon = yt_interpol_linear_2(table2array(Track(:,{'JulDate','Lat',...
+                    'Lon'})),Dive1Stat.JulDate(:));
+                %Add lat to dive statistics
+                Dive1Stat.Lat=Dive1LatLon(:,2);
+                %Add lon to dive statistics
+                Dive1Stat.Lon=Dive1LatLon(:,3);
+                %Lat/lon errors from linear interpolation of processed track based on time
+                Dive1LatLonSE = yt_interpol_linear_2(table2array(Track(:,{'JulDate','y_se_km',...
+                    'x_se_km'})),Dive1Stat.JulDate(:));
+                %Add lat_se to dive statistics
+                Dive1Stat.Lat_se_km=Dive1LatLonSE(:,2);
+                %Add lon_se to dive statistics
+                Dive1Stat.Lon_se_km=Dive1LatLonSE(:,3);
+                %Calculate solar elevation from lat/lon/date/time of each
+                %dive
+                [Az El]=SolarAzEl(datestr(Dive1Stat.JulDate,'yyyy/mm/dd HH:MM:SS'),...
+                    Dive1Stat.Lat,Dive1Stat.Lon,0);
+                %Add solar elevation to dive statistics
+                Dive1Stat.SolarEl=El;
+                %Write dive statistics back to file
+                writetable(Dive1Stat,tdr2file(j));
+                clear Az El
+            end
+            clear j
+        end
+
+        %Repeat the above steps for subsampled TDR data files
+        if ~isempty(tdr2subfile)
+            for j=1:size(tdr2subfile,1)
+                %load dive statistics file
+                Dive1Stat=readtable(tdr2subfile(j));
+                %Lat/lon from linear interpolation of processed track based on time
+                Dive1LatLon = yt_interpol_linear_2(table2array(Track(:,{'JulDate','Lat',...
+                    'Lon'})),Dive1Stat.JulDate(:));
+                %Add lat to dive statistics
+                Dive1Stat.Lat=Dive1LatLon(:,2);
+                %Add lon to dive statistics
+                Dive1Stat.Lon=Dive1LatLon(:,3);
+                %Lat/lon errors from linear interpolation of processed track based on time
+                Dive1LatLonSE = yt_interpol_linear_2(table2array(Track(:,{'JulDate','y_se_km',...
+                    'x_se_km'})),Dive1Stat.JulDate(:));
+                %Add lat_se to dive statistics
+                Dive1Stat.Lat_se_km=Dive1LatLonSE(:,2);
+                %Add lon_se to dive statistics
+                Dive1Stat.Lon_se_km=Dive1LatLonSE(:,3);
+                %Calculate solar elevation from lat/lon/date/time of each
+                %dive
+                [Az El]=SolarAzEl(datestr(Dive1Stat.JulDate,'yyyy/mm/dd HH:MM:SS'),...
+                    Dive1Stat.Lat,Dive1Stat.Lon,0);
+                %Add solar elevation to dive statistics
+                Dive1Stat.SolarEl=El;
+                %Write dive statistics back to file
+                writetable(Dive1Stat,tdr2subfile(j));
+                clear Az El
+            end
+            clear j
+        end
+                %load DiveStat files and interpolate track to it
+        if ~isempty(tdr3file)
+            for j=1:size(tdr3file,1)
+                %load dive statistics file
+                Dive1Stat=readtable(tdr3file(j));
+                %Lat/lon from linear interpolation of processed track based on time
+                Dive1LatLon = yt_interpol_linear_2(table2array(Track(:,{'JulDate','Lat',...
+                    'Lon'})),Dive1Stat.JulDate(:));
+                %Add lat to dive statistics
+                Dive1Stat.Lat=Dive1LatLon(:,2);
+                %Add lon to dive statistics
+                Dive1Stat.Lon=Dive1LatLon(:,3);
+                %Lat/lon errors from linear interpolation of processed track based on time
+                Dive1LatLonSE = yt_interpol_linear_2(table2array(Track(:,{'JulDate','y_se_km',...
+                    'x_se_km'})),Dive1Stat.JulDate(:));
+                %Add lat_se to dive statistics
+                Dive1Stat.Lat_se_km=Dive1LatLonSE(:,2);
+                %Add lon_se to dive statistics
+                Dive1Stat.Lon_se_km=Dive1LatLonSE(:,3);
+                %Calculate solar elevation from lat/lon/date/time of each
+                %dive
+                [Az El]=SolarAzEl(datestr(Dive1Stat.JulDate,'yyyy/mm/dd HH:MM:SS'),...
+                    Dive1Stat.Lat,Dive1Stat.Lon,0);
+                %Add solar elevation to dive statistics
+                Dive1Stat.SolarEl=El;
+                %Write dive statistics back to file
+                writetable(Dive1Stat,tdr3file(j));
+                clear Az El
+            end
+            clear j
+        end
+
+        %Repeat the above steps for subsampled TDR data files
+        if ~isempty(tdr3subfile)
+            for j=1:size(tdr3subfile,1)
+                %load dive statistics file
+                Dive1Stat=readtable(tdr3subfile(j));
+                %Lat/lon from linear interpolation of processed track based on time
+                Dive1LatLon = yt_interpol_linear_2(table2array(Track(:,{'JulDate','Lat',...
+                    'Lon'})),Dive1Stat.JulDate(:));
+                %Add lat to dive statistics
+                Dive1Stat.Lat=Dive1LatLon(:,2);
+                %Add lon to dive statistics
+                Dive1Stat.Lon=Dive1LatLon(:,3);
+                %Lat/lon errors from linear interpolation of processed track based on time
+                Dive1LatLonSE = yt_interpol_linear_2(table2array(Track(:,{'JulDate','y_se_km',...
+                    'x_se_km'})),Dive1Stat.JulDate(:));
+                %Add lat_se to dive statistics
+                Dive1Stat.Lat_se_km=Dive1LatLonSE(:,2);
+                %Add lon_se to dive statistics
+                Dive1Stat.Lon_se_km=Dive1LatLonSE(:,3);
+                %Calculate solar elevation from lat/lon/date/time of each
+                %dive
+                [Az El]=SolarAzEl(datestr(Dive1Stat.JulDate,'yyyy/mm/dd HH:MM:SS'),...
+                    Dive1Stat.Lat,Dive1Stat.Lon,0);
+                %Add solar elevation to dive statistics
+                Dive1Stat.SolarEl=El;
+                %Write dive statistics back to file
+                writetable(Dive1Stat,tdr3subfile(j));
+                clear Az El
+            end
+            clear j
+        end
+    %if there is no tracking data, create lat/lon/solarEl variables in TDR
+    %files and population with NaN
+    else
+        if ~isemtpy(tdr1file)
+            Dive1Stat=readtable(tdr1file);
+            Dive1Stat.Lat(:)=NaN;
+            Dive1Stat.Lon(:)=NaN;
+            Dive1Stat.SolarEl(:)=NaN;
+            writematrix(Dive1Stat,tdr1file);
+        end
+
+        if ~isemtpy(tdr1Subfile)
+            Dive1SubStat=readtable(tdr1Subfile);
+            Dive1SubStat.Lat(:)=NaN;
+            Dive1SubStat.Lon(:)=NaN;
+            Dive1SubStat.SolarEl(:)=NaN;
+            writematrix(Dive1SubStat,tdr1Subfile);
+        end
+
+        if ~isemtpy(tdr2file)
+            Dive2Stat=readtable(tdr2file);
+            Dive2Stat.Lat(:)=NaN;
+            Dive2Stat.Lon(:)=NaN;
+            Dive2Stat.SolarEl(:)=NaN;
+            writematrix(Dive2Stat,tdr2file);
+        end
+
+        if ~isemtpy(tdr2Subfile)
+            Dive2SubStat=readtable(tdr2Subfile);
+            Dive2SubStat.Lat(:)=NaN;
+            Dive2SubStat.Lon(:)=NaN;
+            Dive2SubStat.SolarEl(:)=NaN;
+            writematrix(Dive2SubStat,tdr2Subfile);
+        end
+
+        if ~isemtpy(tdr3file)
+            Dive3Stat=readtable(tdr3file);
+            Dive3Stat.Lat(:)=NaN;
+            Dive3Stat.Lon(:)=NaN;
+            Dive3Stat.SolarEl(:)=NaN;
+            writematrix(Dive3Stat,tdr3file);
+        end
+
+        if ~isemtpy(tdr3Subfile)
+            Dive3SubStat=readtable(tdr3Subfile);
+            Dive3SubStat.Lat(:)=NaN;
+            Dive3SubStat.Lon(:)=NaN;
+            Dive3SubStat.SolarEl(:)=NaN;
+            writematrix(Dive3SubStat,tdr3Subfile);
+
+        end
     end
     clear tdr1file tdr1subfile trackfile
 end
 
-
-
-            if ~isemtpy(tdr1Subfile)
-                Dive1SubStat=readtable(tdr1Subfile);
-                Dive1SubLatLon = yt_interpol_linear_2(table2array(Track(:,{'JulDate','Lat',...
-                    'Lon'})),Dive1SubStat.JulDate(:));
-                Dive1SubStat.Lat=Dive1SubLatLon(:,2);
-                Dive1SubStat.Lon=Dive1SubLatLon(:,3);
-                [Az El]=SolarAzEl(datestr(Dive1SubStat.JulDate,'yyyy/mm/dd HH:MM:SS'),...
-                    Dive1SubStat.Lat,Dive1SubStat.Lon,0);
-                Dive1SubStat.SolarEl=El;
-                writematrix(Dive1SubStat,tdr1Subfile);
-                clear Az El
-            end
-            
-            if ~isemtpy(tdr2file)
-                Dive2Stat=readtable(tdr2file);
-                Dive2LatLon = yt_interpol_linear_2(table2array(Track(:,{'JulDate','Lat',...
-                    'Lon'})),Dive2Stat.JulDate(:));
-                Dive2Stat.Lat=Dive2LatLon(:,2);
-                Dive2Stat.Lon=Dive2LatLon(:,3);
-                [Az El]=SolarAzEl(datestr(Dive2Stat.JulDate,'yyyy/mm/dd HH:MM:SS'),...
-                    Dive2Stat.Lat,Dive2Stat.Lon,0);
-                Dive2Stat.SolarEl=El;
-                writematrix(Dive2Stat,tdr2file);
-                clear Az El
-            end
-            
-            if ~isemtpy(tdr2Subfile)
-                Dive2StatSub=readtable(tdr2Subfile);
-                Dive2SubLatLon = yt_interpol_linear_2(table2array(Track(:,{'JulDate','Lat',...
-                    'Lon'})),Dive2SubStat.JulDate(:));
-                Dive2SubStat.Lat=Dive2SubLatLon(:,2);
-                Dive2SubStat.Lon=Dive2SubLatLon(:,3);
-                [Az El]=SolarAzEl(datestr(Dive2SubStat.JulDate,'yyyy/mm/dd HH:MM:SS'),...
-                    Dive2SubStat.Lat,Dive2SubStat.Lon,0);
-                Dive2SubStat.SolarEl=El;
-                writematrix(Dive2SubStat,tdr2Subfile);
-                clear Az El
-            end
-            
-            if ~isemtpy(tdr3file)
-                Dive3Stat=readtable(tdr3file);
-                
-                Dive3LatLon = yt_interpol_linear_2(table2array(Track(:,{'JulDate','Lat',...
-                    'Lon'})),Dive3Stat.JulDate(:));
-                Dive3Stat.Lat=Dive3LatLon(:,2);
-                Dive3Stat.Lon=Dive3LatLon(:,3);
-                [Az El]=SolarAzEl(datestr(Dive3Stat.JulDate,'yyyy/mm/dd HH:MM:SS'),...
-                    Dive3Stat.Lat,Dive3Stat.Lon,0);
-                Dive3Stat.SolarEl=El;
-                writematrix(Dive3Stat,tdr3file);
-                clear Az El
-            end
-            
-            if ~isemtpy(tdr3Subfile)
-                Dive3StatSub=readtable(tdr3Subfile);
-                Dive3SubLatLon = yt_interpol_linear_2(table2array(Track(:,{'JulDate','Lat',...
-                    'Lon'})),Dive3SubStat.JulDate(:));
-                Dive3SubStat.Lat=Dive3SubLatLon(:,2);
-                Dive3SubStat.Lon=Dive3SubLatLon(:,3);
-                [Az El]=SolarAzEl(datestr(Dive3SubStat.JulDate,'yyyy/mm/dd HH:MM:SS'),...
-                    Dive3SubStat.Lat,Dive3SubStat.Lon,0);
-                Dive3SubStat.SolarEl=El;
-                writematrix(Dive3SubStat,tdr3Subfile);
-                clear Az El
-            end
-        %if there is no tracking data, create lat/lon/solarEl variables in TDR 
-        %files and population with NaN
-        else
-            if ~isemtpy(tdr1file)
-                Dive1Stat=readtable(tdr1file);
-                Dive1Stat.Lat(:)=NaN;
-                Dive1Stat.Lon(:)=NaN;
-                Dive1Stat.SolarEl(:)=NaN;
-                writematrix(Dive1Stat,tdr1file);
-                
-            end
-            if ~isemtpy(tdr1Subfile)
-                Dive1SubStat=readtable(tdr1Subfile);
-                Dive1SubStat.Lat(:)=NaN;
-                Dive1SubStat.Lon(:)=NaN;
-                Dive1SubStat.SolarEl(:)=NaN;
-                writematrix(Dive1SubStat,tdr1Subfile);
-                
-            end
-            if ~isemtpy(tdr2file)
-                Dive2Stat=readtable(tdr2file);
-                Dive2Stat.Lat(:)=NaN;
-                Dive2Stat.Lon(:)=NaN;
-                Dive2Stat.SolarEl(:)=NaN;
-                writematrix(Dive2Stat,tdr2file);
-            end
-            
-            if ~isemtpy(tdr2Subfile)
-                Dive2SubStat=readtable(tdr2Subfile);
-                Dive2SubStat.Lat(:)=NaN;
-                Dive2SubStat.Lon(:)=NaN;
-                Dive2SubStat.SolarEl(:)=NaN;
-                writematrix(Dive2SubStat,tdr2Subfile);
-            end
-            if ~isemtpy(tdr3file)
-                Dive3Stat=readtable(tdr3file);
-                Dive3Stat.Lat(:)=NaN;
-                Dive3Stat.Lon(:)=NaN;
-                Dive3Stat.SolarEl(:)=NaN;
-                writematrix(Dive3Stat,tdr3file);
-            end
-            if ~isemtpy(tdr3Subfile)
-                Dive3SubStat=readtable(tdr3Subfile);
-                Dive3SubStat.Lat(:)=NaN;
-                Dive3SubStat.Lon(:)=NaN;
-                Dive3SubStat.SolarEl(:)=NaN;
-                writematrix(Dive3SubStat,tdr3Subfile);
-                
-            end
-        end
-            
-end
